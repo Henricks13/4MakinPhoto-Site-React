0f39d6341fc0b428ab9310fa96b382c0600929c8
Ryan Henrick Da Silva Pinto <henrick.s1320@gmail.com>
2024-03-28 15:05:35 -0300

    Admin Login

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 // App.js
 import 'bootstrap/dist/css/bootstrap.min.css';
 import React, { useState, useEffect } from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'; // Troque de 'Switch' para 'Routes'
 import Header from './components/Header';
 import Footer from './components/Footer';
 import HomePage from './components/HomePage';
 import AllGamesPage from './components/AllGamesPage';
 import ContactPage from './components/ContactPage'; // Importe a página de contato
+import AdminLoginPage from './components/AdminLoginPage'; // Importe a página de login do administrador
 import Spinner from './components/Spinner';
 
 function App() {
@@ -17,11 +18,9 @@ function App() {
       setLoading(false);
     };
 
-    // Quando o conteúdo da página terminar de carregar, pare de exibir o spinner
     window.addEventListener('load', handleLoad);
 
     return () => {
-      // Remova o ouvinte de evento quando o componente é desmontado
       window.removeEventListener('load', handleLoad);
     };
   }, []);
@@ -34,7 +33,8 @@ function App() {
           <Routes>
             <Route path="/" element={<HomePage />} />
             <Route path="/all-games" element={<AllGamesPage />} />
-            <Route path="/contact" element={<ContactPage />} /> {/* Adicione a rota para a página de contato */}
+            <Route path="/contact" element={<ContactPage />} />
+            <Route path="/admin-login" element={<AdminLoginPage />} />
           </Routes>
         </div>
         <Footer />
--- src/components/AdminLoginPage.js
+++ src/components/AdminLoginPage.js
@@ -1,12 +1,59 @@
 // AdminLoginPage.js
-import React from 'react';
+
+import React, { useState } from 'react';
+import './AdminLoginPage.css'; // Arquivo de estilos CSS para a página de login
 
 function AdminLoginPage() {
-  // Adicione o código para a página de login do administrador aqui
+  // Definindo estado para armazenar os valores do formulário
+  const [formData, setFormData] = useState({
+    username: '',
+    password: ''
+  });
+
+  // Função para atualizar o estado quando os campos do formulário mudarem
+  const handleInputChange = (event) => {
+    const { name, value } = event.target;
+    setFormData({
+      ...formData,
+      [name]: value
+    });
+  };
+
+  // Função para lidar com o envio do formulário
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    // Aqui você pode adicionar a lógica para enviar os dados de login para o backend e autenticar o usuário
+    console.log('Formulário enviado:', formData);
+  };
+
   return (
-    <div>
-      <h1>Página de Login do Administrador</h1>
-      {/* Adicione o formulário de login aqui */}
+    <div className="admin-login-page">
+      <h2>Login Administrativo</h2>
+      <form onSubmit={handleSubmit}>
+        <div className="form-group">
+          <label htmlFor="username">Nome de Usuário:</label>
+          <input
+            type="text"
+            id="username"
+            name="username"
+            value={formData.username}
+            onChange={handleInputChange}
+            required
+          />
+        </div>
+        <div className="form-group">
+          <label htmlFor="password">Senha:</label>
+          <input
+            type="password"
+            id="password"
+            name="password"
+            value={formData.password}
+            onChange={handleInputChange}
+            required
+          />
+        </div>
+        <button type="submit">Entrar</button>
+      </form>
     </div>
   );
 }
